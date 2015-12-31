d19196050c302831147a8258bea68364814c24ec
James Jenkins <applesmacs@yahoo.com>
2015-12-31 12:15:09 -0500

    Overload <, > for chromosome; Add tests and a prop
    - Simple test for chromosome overloaded opperators
    - Adds a "is pro" bool to chromosome

--- chromosome.py
+++ chromosome.py
@@ -1,7 +1,16 @@
+#!/usr/bin/env python2
+
 class Chromosome:
     def __init__(self, genes = [], score = 0):
         self.genes = genes
         self.score = score
+        self.is_top_performer = False
+
+    def __gt__(self, other):
+        return self.score > other.score
+
+    def __lt__(self, other):
+        return self.score < other.score
 
     def append(self, value):
         self.genes.append(value)
@@ -14,3 +23,12 @@ class Chromosome:
 
     def __len__(self):
         return len(self.genes)
+
+
+if __name__ == "__main__":
+    c = Chromosome(score = 1)
+    d = Chromosome(score = 2)
+    print(c > d)
+    print(d > c)
+    print(c < d)
+    print(d < c)
