419ed95e60b186d9e094020ac25cd1e65cdef81f
James Jenkins <applesmacs@yahoo.com>
2015-12-31 12:42:05 -0500

    Improve genetic alg, breeding, roulette selection
    - Add different type of breeding (crossover point)
    - Do some eliteism by adding copies of previous best performers
    - Change roulette selection method to mirror ai-junki.com stuff
    - Adjusted how perturbation works

--- ga.py
+++ ga.py
@@ -3,6 +3,8 @@ from copy import copy as _copy
 
 from chromosome import Chromosome
 
+
+# Gene mixing (every gene) type breeding
 class DistBreeder(object):
     def __init__(self, crossover_rate, mutation_rate):
         self.crossover_rate = crossover_rate
@@ -21,40 +23,80 @@ class DistBreeder(object):
 
         return child1, child2
 
-#    def BreedCross(self, parent1, parent2):
+
+# "Crossover" type chromosome breeding
+class SwitchBreeder(object):
+    def __init__(self, crossover_rate, mutation_rate):
+        self.crossover_rate = crossover_rate
+        self.mutation_rate = mutation_rate
+
+    def Breed(self, mom, dad):
+        # Shall we do crossover?
+        if random.random() > self.crossover_rate and mom == dad:
+            return mom, dad
+
+        # Pick crossover point
+        i = random.randrange(0, len(mom))
+
+        child1 = mom[:i]
+        child1.extend(dad[i:])
+
+        child2 = dad[:i]
+        child2.extend(dad[i:])
+
+        return Chromosome(child1), Chromosome(child2)
 
 
-# Mutation rate is suggested to between 0.05 and 0.2
-class RealGeneticAlg(DistBreeder):
+# Mutation rate is suggested to between 0.05 and 0.2 (from ai-junki.com) for
+# real number alleles
+class RealGeneticAlg(SwitchBreeder):
     def __init__(self, crossover_rate = 0.5, mutation_rate = 0.1,
-                    perturbation_bounds = (0.05, 0.1)):
+                       perturbation_bounds = (0.05, 0.1), elite = 4):
         super(RealGeneticAlg, self).__init__(crossover_rate, mutation_rate)
 
         self.perturbation_bounds = perturbation_bounds
+        self.n_elite = elite
 
     def NewPopulation(self, old_population):
+        old_population = sorted(old_population)
         new_population = []
-        try:
-            while len(new_population) < len(old_population):
-                # Select two parents
-                mom = RouletteSelect(old_population)
-                dad = RouletteSelect(old_population)
+        # Do some elitism
+        new_population.extend(old_population[-self.n_elite:])
+        # Mark these old best performers as such
+        for c in new_population:
+            c.is_top_performer = True
 
-                # Breed parents, mutate and add both children to new population
-                new_population.extend(map(self.Mutate,self.Breed(mom, dad)))
+        # Build up a new population
+        while len(new_population) < len(old_population):
+            # Select two parents
+            mom = RouletteSelect(old_population)
+            dad = RouletteSelect(old_population)
 
-            return new_population
-        except:
-            print old_population
+            # Breed parents, mutate and add both children to new population
+            new_population.extend(map(self.Mutate,self.Breed(mom, dad)))
+
+        return new_population
 
     def Mutate(self, chromosome):
         for i in xrange(len(chromosome)):
-            chromosome[i] += random.choice((1, -1)) *\
-                             ClampRandom(*self.perturbation_bounds)
+            chromosome[i] += random.choice((1, -1)) * \
+                             random.random()*self.perturbation_bounds[1]
+                             #ClampRandom(*self.perturbation_bounds)
         return chromosome
 
 
+# Based off code at ai-junki.com
 def RouletteSelect(population):
+    total = sum(map(lambda x: x.score, population))
+    pie_slice = random.random() * total
+    last = 0
+    for chromosome in population:
+        last += chromosome.score
+        if last >= pie_slice:
+            return chromosome
+
+
+def MyRouletteSelect(population):
     n = random.random()
     last = 0
     total = sum(map(lambda x: x.score, population))
