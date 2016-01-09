f808a05db9612605b901228e0a2215fc945b474b
James Jenkins <applesmacs@yahoo.com>
2016-01-08 21:41:50 -0500

    Add poision class

--- food.py
+++ food.py
@@ -2,6 +2,7 @@ import pyglet as _pyglet
 from vector import Vector as _Vector
 
 FOOD_IMAGE = "resources/images/food.png"
+POISON_IMAGE = "resources/images/poison.png"
 
 class Food(_pyglet.sprite.Sprite):
     # Load food image resource
@@ -17,11 +18,28 @@ class Food(_pyglet.sprite.Sprite):
         self.pos = _Vector(pos[0], pos[1], True)
         self.x, self.y = self.pos
 
-        self.parent_window = parent_window
         self.eaten = False
 
-    def update(self, dt):
-        pass
+    def collidesWith(self, ant):
+        if (ant.pos-self.pos).magnitude < (ant.radius+self.radius):
+            return True
+        else:
+            return False
+
+
+class Poison(_pyglet.sprite.Sprite):
+    # Load food image resource
+    image = _pyglet.resource.image(POISON_IMAGE)
+    image.anchor_x = image.width/2
+    image.anchor_y = image.height/2
+
+    def __init__(self,parent_window, pos, batch=None):
+        super(Poison, self).__init__(Poison.image, pos[0], pos[1], batch=batch)
+        self.scale = 1.25
+        self.radius = Food.image.width/2 * self.scale
+
+        self.pos = _Vector(pos[0], pos[1], True)
+        self.x, self.y = self.pos
 
     def collidesWith(self, ant):
         if (ant.pos-self.pos).magnitude < (ant.radius+self.radius):
